332b3d717fa89bc1dbcbf2567fd02ebbea21c3e1
rianabd01 <rianwahyuab@gmail.com>
2024-10-12 00:04:46 +0700

    feat: customizable classname

--- src/components/layouts/ImageUploader.tsx
+++ src/components/layouts/ImageUploader.tsx
@@ -2,12 +2,17 @@
 "use client";
 import React, { useRef, useState } from "react";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 interface ImageUploaderProps {
-  onFileSelect: (file: File) => void; // Prop to pass selected file
+  onFileSelect: (file: File) => void;
+  className?: string;
 }
 
-const ImageUploader: React.FC<ImageUploaderProps> = ({ onFileSelect }) => {
+const ImageUploader: React.FC<ImageUploaderProps> = ({
+  onFileSelect,
+  className,
+}) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
@@ -16,13 +21,16 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onFileSelect }) => {
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       setSelectedImage(imageUrl);
-      onFileSelect(file); // Send the selected file to parent
+      onFileSelect(file);
     }
   };
 
   return (
     <div
-      className="relative w-20 h-20 border-dotted border-2 border-black flex items-center justify-center text-2xl cursor-pointer"
+      className={cn(
+        "relative w-20 h-20 border-dotted border-2 border-black flex items-center justify-center text-2xl cursor-pointer",
+        className
+      )}
       onClick={() => inputRef.current?.click()}
     >
       {selectedImage ? (
@@ -31,7 +39,7 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onFileSelect }) => {
           alt="Uploaded image"
           width={200}
           height={200}
-          className="object-cover"
+          className="object-cover h-full aspect-square"
         />
       ) : (
         "+"
