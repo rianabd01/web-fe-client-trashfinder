4d77df5a8876acbf23f6ac3c9a2b16be5a526a8b
rianabd01 <rianwahyuab@gmail.com>
2024-10-12 00:05:31 +0700

    feat: add more input form

--- src/app/(main)/upload/page.tsx
+++ src/app/(main)/upload/page.tsx
@@ -3,6 +3,16 @@ import React, { useState } from "react";
 import ContainerWrap from "@/components/layouts/ContainerWrap";
 import ToPreviousPageButton from "@/app/(main)/components/ToPreviousPageButton";
 import ImageUploader from "@/components/layouts/ImageUploader";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 export default function UploadPage() {
   const [files, setFiles] = useState<{ [key: number]: File | null }>({
@@ -35,11 +45,14 @@ export default function UploadPage() {
     validFiles.forEach((file, index) => {
       if (file) {
         formData.append(`file-${index}`, file);
-        console.log(file);
+        console.log(title, description);
       }
     });
     // alert("Please select images");
   };
+
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
 
   return (
     <div className="min-h-dvh space-y-10">
@@ -52,35 +65,87 @@ export default function UploadPage() {
       <ContainerWrap>
         <div className="container flex flex-row justify-center space-x-4">
           {/* ImageUploader components with index passed */}
-          <ImageUploader onFileSelect={(file) => handleFileSelect(0, file)} />
-          <ImageUploader onFileSelect={(file) => handleFileSelect(1, file)} />
-          <ImageUploader onFileSelect={(file) => handleFileSelect(2, file)} />
+          <ImageUploader
+            onFileSelect={(file) => handleFileSelect(0, file)}
+            className="w-32 h-32"
+          />
+          <ImageUploader
+            onFileSelect={(file) => handleFileSelect(1, file)}
+            className="w-32 h-32"
+          />
+          <ImageUploader
+            onFileSelect={(file) => handleFileSelect(2, file)}
+            className="w-32 h-32"
+          />
         </div>
       </ContainerWrap>
 
       <ContainerWrap>
         <div className="container">
           <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
-            <div className="inline-flex gap-5">
-              <label htmlFor="title" className="flex flex-col w-2/6">
+            <div className="grid grid-cols-4">
+              <label htmlFor="title" className="flex flex-col col-span-1">
                 Sampah apa yang kamu temukan{" "}
                 <span className="text-sm leading-2 text-gray-700">
                   Kasih judul yang cocok
                 </span>
               </label>
-              <input
+              <Input
                 type="text"
                 id="title"
-                className="w-4/6 border border-gray-900 rounded focus:outline-none focus:ring-1 focus:ring-black focus:ring-offset-1 px-2"
+                className="col-span-3 border border-gray-900 rounded focus:outline-none focus:ring-1 focus:ring-black focus:ring-offset-1 px-2"
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
-
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded w-max"
-              type="submit"
-            >
-              Upload Files
-            </button>
+            <div className="grid grid-cols-4">
+              <label htmlFor="description" className="flex flex-col col-span-1">
+                Deskripsikan sampahnya{" "}
+                <span className="text-sm leading-2 text-gray-700">
+                  Jelaskan sampah itu
+                </span>
+              </label>
+              <Textarea
+                id="description"
+                className="col-span-3 border border-gray-900 rounded focus:outline-none focus:ring-1 focus:ring-black focus:ring-offset-1 px-2"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="grid grid-cols-4">
+              <label htmlFor="loc" className="flex flex-col col-span-1">
+                Titik lokasi{" "}
+                <span className="text-sm leading-2 text-gray-700">
+                  Bagikan titik lokasi pada aplikasi maps kamu
+                </span>
+              </label>
+              <Input
+                type="text"
+                id="loc"
+                className="col-span-3 border border-gray-900 rounded focus:outline-none focus:ring-1 focus:ring-black focus:ring-offset-1 px-2"
+              />
+            </div>
+            <div className="grid grid-cols-4">
+              <label htmlFor="title" className="flex flex-col col-span-1">
+                Wilayah{" "}
+                <span className="text-sm leading-2 text-gray-700">
+                  Dimana kamu menemukan sampah ini
+                </span>
+              </label>
+              <Select>
+                <SelectTrigger className="w-full col-span-1 border-black">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="grid grid-cols-4">
+              <Button type="submit" className="col-start-2 w-max">
+                Unggah
+              </Button>
+            </div>
           </form>
         </div>
       </ContainerWrap>
