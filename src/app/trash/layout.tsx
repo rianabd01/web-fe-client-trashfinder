f84854843263719dc37618f64bddf8875011e907
rianabd01 <rianwahyuab@gmail.com>
2024-10-11 07:07:46 +0700

    feat: add layout for detail

--- src/app/trash/layout.tsx
+++ src/app/trash/layout.tsx
@@ -1,29 +1,10 @@
-import Link from "next/link";
 import React from "react";
+const Navbar = React.lazy(() => import("@/components/layouts/navbar"));
 
-export default function Layout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className="bg-gray-700 w-full h-20">
-        <div className="container m-auto w-full h-full flex flex-row justify-between items-center text-white">
-          <div>LOGO</div>
-          <nav className="flex flex-row gap-5">
-            <Link href={"#"} className="cursor-pointer">
-              home
-            </Link>
-            <Link href={"#"} className="cursor-pointer">
-              login
-            </Link>
-            <Link href={"#"} className="cursor-pointer">
-              upload
-            </Link>
-          </nav>
-        </div>
-      </header>
+      <Navbar />
       {children}
     </>
   );
