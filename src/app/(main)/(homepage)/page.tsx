c2d3a0a864b5c508c8614afc624d7b1db9a0472f
rianabd01 <rianwahyuab@gmail.com>
2024-10-12 10:00:22 +0700

    feat: add image about us

--- src/app/(main)/(homepage)/page.tsx
+++ src/app/(main)/(homepage)/page.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import ContainerWrap from "@/components/layouts/ContainerWrap";
 import SliderTrashList from "./components/SliderTrashList";
 import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 export default function Home() {
+  const aboutUsImage = [
+    "/images/clean-park.jpg",
+    "/images/clean-sea.jpg",
+    "/images/clean-river.jpg",
+  ];
   return (
     <>
       <div className="min-h-dvh flex flex-col items-center">
@@ -15,16 +26,35 @@ export default function Home() {
           </div>
         </ContainerWrap>
         <ContainerWrap>
-          <div className="container flex flex-row items-center gap-5">
-            <Image
-              src={"/images/no-image.png"}
-              alt=""
-              width={900}
-              height={900}
-              className="w-3/6 h-2/4 object-cover"
-            />
+          <div className="container flex flex-col md:flex-row items-center gap-5">
+            <Carousel
+              className="w-3/6 h-2/4"
+              opts={{
+                loop: true,
+              }}
+            >
+              <CarouselContent>
+                {aboutUsImage.map((item, index) => (
+                  <CarouselItem key={index}>
+                    <div className="p-1">
+                      <Card className="border-none">
+                        <CardContent className="flex aspect-square items-center justify-center p-0">
+                          <Image
+                            src={item}
+                            alt=""
+                            width={900}
+                            height={900}
+                            className="object-cover w-full aspect-square"
+                          />
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
             <div className="flex flex-col gap-3">
-              <h4 className="font-bold text-2xl">Impian kami</h4>
+              <h4 className="font-bold text-2xl text-center">Impian kami</h4>
               <p className="font-medium">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
                 iusto iste ducimus nisi iure reiciendis? Consequuntur, nobis
